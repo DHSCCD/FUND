997996a2e580e9cca7b764cdb2aff8f6a960b64b
DongHyun <zzuner94@naver.com>
2022-09-04 01:36:36 +0900

    1

--- Bull.py
+++ Bull.py
@@ -1,17 +1,18 @@
 import pyupbit
 import pandas as pd
 
-BTC = pyupbit.get_ohlcv('KRW-BTC', interval="day",count=39)
-pd.set_option('display.float_format' ,lambda x: f'{x:.2df}')
+BTC = pyupbit.get_ohlcv('KRW-BTC')
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
 #불린저 밴더 = 20일 이평선 - (20일 표준편차*곱)
 
 #1. 20일 이평선
-BTC['BTC_20'] = BTC['close'].rolling(20).mean()
+BTC['20일_이평선'] = BTC['close'].rolling(20).mean()
 
 #2. 20일 표준편차
-BTC['BTC__20'] = BTC['close'].rolling(20).std()
+BTC['표준편차']= BTC['close'].rolling(20).std()
 
 #3. 볼린저밴드 20일 기준
-print(BTC['BTC_20'])
-
-
+BTC['볼린저밴드_하한가'] = BTC['20일_이평선'] - 2*BTC['표준편차']
+print(BTC)
+print(BTC['20일_이평선'])
